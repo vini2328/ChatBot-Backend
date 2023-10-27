b3235d40f2bc0126cd47d44232db0f81ecf05f38
vini2328 <123352754+vini2328@users.noreply.github.com>
2023-10-27 18:04:16 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -121,7 +121,7 @@ class UserController {
                     const secret = user._id +process.env.JWT_SECRET_KEY
     
                     const token =JWT.sign({userID :user._id},secret,{expiresIn:"15m"})
-                    const link = `https://653baac420753b35a19149c0--luxury-douhua-fa0d43.netlify.app/confirmpassword/${user._id}/${token}`
+                    const link = `https://653bae14b19f402c05d0847d--willowy-sprite-183245.netlify.app/confirmpassword/${user._id}/${token}`
                     console.log(link)
     
                     // send email
